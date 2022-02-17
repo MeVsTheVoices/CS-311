380cb9714aa3a35a2221371590586dd7d5d3b238
Joshua Dunne <mevsthevoices@gmail.com>
2022-02-17 07:50:24 -0800

    final
    finished

--- CS-311-Homework-3/doc.go
+++
@@ -1,6 +0,0 @@
-// CS-311-Homework-4 project doc.go
-
-/*
-CS-311-Homework-4 document
-*/
-package main
--- CS-311-Homework-3/section.go
+++ CS-311-Homework-3/section.go
@@ -15,6 +15,20 @@ type Section struct {
 	mStudentNames []string
 }
 
+//function to print all students in  a section
+func (sec Section) String() string {
+	//declare an empty string
+	var concatanatedNames string
+
+	//for each student in the section add it and a newline
+	for _, j := range sec.mStudentNames {
+		concatanatedNames = concatanatedNames + "\n" + j
+	}
+
+	//return the created string to the caller
+	return concatanatedNames
+}
+
 //Struct assosciating a student by name to a particular section
 type AlreadyEnrolled struct {
 	//name of the student
@@ -26,7 +40,7 @@ type AlreadyEnrolled struct {
 //provide an interface for AlreadyEnrolled so that it can be reduced to a string
 //and returned as an error for register
 func (student AlreadyEnrolled) Error() string {
-	return student.mStudentName + " is already in " + student.mSection.mCourseNumber + "-" + student.mSection.mSectionNumber
+	return student.mStudentName + " is already in " + student.mSection.mDepartment + " " + student.mSection.mCourseNumber + "-" + student.mSection.mSectionNumber
 }
 
 //try to add a student 'name' to a section 'sec'
@@ -53,5 +67,46 @@ func (this *Section) register(name string) (bool, error) {
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	//print out a header
+	fmt.Println("Simulates basics of class registration")
+
+	//create a section
+	//department cs, course number 101, section number 1
+	//give it an empty string to start filling students names into
+	aSection := Section{"cs", "101", "1", []string{}}
+
+	//create two strings, one for yes/no, one for student's name
+	var studentName, doContinue string
+	for {
+		//print prompt containing section details
+		fmt.Print("Enter name to enroll in ")
+		fmt.Print(aSection.mDepartment)
+		fmt.Print(" ")
+		fmt.Print(aSection.mCourseNumber)
+		fmt.Print("-")
+		fmt.Print(aSection.mSectionNumber)
+		fmt.Print(": ")
+
+		//grab a student's name
+		fmt.Scan(&studentName)
+
+		//attempt to register that student in the section
+		registerSuccess, registerError := aSection.register(studentName)
+		//if the register function doesn't return true, print the error
+		if !registerSuccess {
+			fmt.Println(registerError)
+		}
+
+		//ask the user whether to continue or not
+		fmt.Print("Do you want to quit? (yes/no): ")
+		fmt.Scan(&doContinue)
+		if doContinue == "yes" {
+			//anything not explicitily "yes" continues entering students
+			break
+		}
+	}
+
+	//dump the final list of students
+	fmt.Print("The final list of students is:")
+	fmt.Println(aSection)
 }
